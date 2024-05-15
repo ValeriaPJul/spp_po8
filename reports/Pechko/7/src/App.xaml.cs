3b3c0aa845958814cfcd656e0c150225b3a33253
ValeriaPJul <ValeriaPJul@users.github.com>
2024-05-15 18:21:44 +0300

    [Pechko] Add lab_7

---
+++ reports/Pechko/7/src/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace sppLab7z1;
+public partial class App : Application
+{
+}
