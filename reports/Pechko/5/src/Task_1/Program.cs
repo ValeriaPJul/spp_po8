c25dc3f7f99c37b563ee4cc0dd3354b7b1160c66
ValeriaPJul <ValeriaPJul@users.github.com>
2024-05-15 15:27:40 +0300

    [Pechko] Add lab_5

---
+++ reports/Pechko/5/src/Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿public interface IBuilding
+{
+    void Open();
+    void Close();
+}
+
+public abstract class PublicBuilding : IBuilding
+{
+    public abstract void Open();
+    public abstract void Close();
+
+    public void PublicService()
+    {
+        Console.WriteLine("Providing public service");
+    }
+}
+
+public class Theatre : PublicBuilding
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening theatre...");
+    }
+
+    public override void Close()
+    {
+        Console.WriteLine("Closing theatre...");
+    }
+
+    public void Play()
+    {
+        Console.WriteLine("Starting play...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Theatre theatre = new Theatre();
+        theatre.Open();
+        theatre.PublicService();
+        theatre.Play();
+        theatre.Close();
+    }
+}
---
+++ reports/Pechko/5/src/Task_2/Program.cs
@@ -0,0 +1,53 @@
+﻿abstract class Learner
+{
+    protected string Name;
+
+    public Learner(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Study();
+}
+
+class Schoolchild : Learner
+{
+    public Schoolchild(string name) : base(name)
+    {
+    }
+
+    public override void Study()
+    {
+        Console.WriteLine(Name + " is studying school subjects");
+    }
+}
+
+class Student : Learner
+{
+    public Student(string name) : base(name)
+    {
+    }
+
+    public override void Study()
+    {
+        Console.WriteLine(Name + " is studying university subjects");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Learner[] learners = {
+            new Schoolchild("Ivan"),
+            new Student("Maria"),
+            new Schoolchild("Petr"),
+            new Student("Anna")
+        };
+
+        foreach (Learner learner in learners)
+        {
+            learner.Study();
+        }
+    }
+}
